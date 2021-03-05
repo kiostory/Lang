a659e78970f32ede3a788ce096d76972cae4d002
kiostory <koreamcp@naver.com>
2021-03-05 11:42:39 +0900

    commit

--- Lang/C/CosPro5/5_code/5_10_solution_code.c
+++ Lang/C/CosPro5/5_code/5_10_solution_code.c
@@ -27,14 +27,14 @@ int* solution(int a, int b) {
     answer[1] = c.minus(a, b);
     
     return answer;
-}
+} 
 
-// 아래는 테스트케이스 출력을 해보기 위한 main 함수입니다.
+// 아래는 테스트케이스 출력을 해보기 위한 main 함수입니다.  
 int main() {
     int a = 3;
     int b = 5;
     int* ret = solution(3, 5);
 
     // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    printf("solution 함수의 반환 값은 {%d, %d} 입니다.\n", ret[0], ret[1]);
+    printf("solution 함수의 반환 값은 {%d, %d} 입니다.\n", ret[0], ret[1]);  //solution 함수의 반환 값은 {8, -2} 입니다.
 }
